63344b07a81ce321678c74060a58c1531a1ce68d
Lokeshwar Deb <lokeshwardebdevelop@gmail.com>
2023-10-06 20:25:18 +0600

    the initializational commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+// initalizing the path of the server
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+
+
+// register routes
+$Routes = [
+
+    "/" => __DIR__ . "/views/pages/views.home.php",
+    "/home" => __DIR__ . "/views/pages/views.home.php",
+
+];
+
+// applying the conditions
+if(array_key_exists($uri, $Routes)){
+    require $Routes[$uri];
+}else{
+    require __DIR__ . '/views/error_pages/views.error.php';
+}
+
+
+
+
+
+
+?>
